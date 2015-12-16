378b3a963d4b61092892576bb0dcdfed07d285d0
davidhickey <davidhickey423@gmail.com>
2015-12-15 19:20:13 -0500

    successful login functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
   root to: 'articles#index'
-  
+
   resources :articles
+  resources :authors
+  resources :author_sessions, only: [ :new, :create, :destroy ]
+
+  get 'login' => 'author_sessions#new'
+  get 'logout' => 'author_sessions#destroy'
+
 end
