7f25b41ee76437824365c5641ea47aa5eca75c74
davidhickey <davidhickey423@gmail.com>
2015-12-18 10:55:37 -0500

    fixed some functionality with creators controller

--- app/controllers/creators_controller.rb
+++ app/controllers/creators_controller.rb
@@ -10,6 +10,7 @@ class CreatorsController < ApplicationController
   # GET /creators/1
   # GET /creators/1.json
   def show
+    @creator = Creator.find(params[:id])
   end
 
   # GET /creators/new
@@ -19,46 +20,52 @@ class CreatorsController < ApplicationController
 
   # GET /creators/1/edit
   def edit
+    @creator = Creator.find(params[:id])
   end
 
   # POST /creators
   # POST /creators.json
   def create
     @creator = Creator.new(creator_params)
+    @creator.save
 
-    respond_to do |format|
-      if @creator.save
-        format.html { redirect_to @creator, notice: 'Creator was successfully created.' }
-        format.json { render :show, status: :created, location: @creator }
-      else
-        format.html { render :new }
-        format.json { render json: @creator.errors, status: :unprocessable_entity }
-      end
-    end
+    # flash.notice "Writer '#{@creator.email}' Created!"
+
+    redirect_to creator_path(@creator)
   end
 
   # PATCH/PUT /creators/1
   # PATCH/PUT /creators/1.json
   def update
-    respond_to do |format|
-      if @creator.update(creator_params)
-        format.html { redirect_to @creator, notice: 'Creator was successfully updated.' }
-        format.json { render :show, status: :ok, location: @creator }
-      else
-        format.html { render :edit }
-        format.json { render json: @creator.errors, status: :unprocessable_entity }
-      end
-    end
+    @creator = Creator.find(params[:id])
+    @creator.update(creator_params)
+
+    # respond_to do |format|
+    #   if @creator.update(creator_params)
+    #     format.html { redirect_to @creator, notice: 'Creator was successfully updated.' }
+    #     format.json { render :show, status: :ok, location: @creator }
+    #   else
+    #     format.html { render :edit }
+    #     format.json { render json: @creator.errors, status: :unprocessable_entity }
+    #   end
+    # end
+
+    flash.notice = "Writer '#{@creator.name}' Updated!"
+
+    redirect_to creator_path(@creator)
   end
 
   # DELETE /creators/1
   # DELETE /creators/1.json
   def destroy
+    @creator = Creator.find(params[:id])
     @creator.destroy
+
     respond_to do |format|
       format.html { redirect_to creators_url, notice: 'Creator was successfully destroyed.' }
       format.json { head :no_content }
     end
+    redirect_to creators_path
   end
 
   private
