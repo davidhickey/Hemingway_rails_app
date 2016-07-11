cafb7bbb926579e7fbf5f2255dcbff66cd9ac9f1
davidhickey <davidhickey423@gmail.com>
2016-07-11 18:41:42 -0400

    takes away js message

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,3 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-
-alert("Hi there! I'm currently in the midst of adding functionality to this site to make it more user-friendly. Stay tuned!")
